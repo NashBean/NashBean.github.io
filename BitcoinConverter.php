1068738507d8e61f56f9c44a1f9f3b5d06e7e53d
NashBean <nashbean@gmail.com>
2015-12-07 03:40:26 -0500

    testing and begugging

--- BitcoinConverter.php
+++ BitcoinConverter.php
@@ -11,3 +11,43 @@
     $btcDisplay = round($btcPrice, 2);
 ?>
 
+<html>
+<header>
+<style>
+h1{
+    font-family: "Calibri", Arial, sans-serif;
+    font-size: 42px;
+}
+#container{
+font-family: "Calibri", Arial, sans-serif;
+font-size: 42px;
+border: 4px solid #999999;
+border-radius: 6px;
+height: 100px;
+width: 500px;
+}
+</style>
+</header>
+<center>
+<body>
+<h1> iBean Bitcoin Exchange </h1>
+
+<div id="btc-quote"></div>
+<script type="text/javascript" src="https://www.weusecoins.com/embed.js"></script>
+
+<div id="container">
+<input type="text" name="bi" 
+onchange="btcConvert(this);" onkeyup="btcConvert(this);"/>
+BTC=<input type="text" name="ci" id="ci" readonly="true"/> USD
+</div>
+<script>
+function btcConvert(input){
+    if(isNaN(input.value)){input.value=0;}
+    var price="<? php echo $btcDisplay; ?>";
+    var output=input.value * price;
+    var co=document.getElementById('ci');
+    ci.value=output.toFixed(2);
+}
+</script>
+<body>
+</html>
